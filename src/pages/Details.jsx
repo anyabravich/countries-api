bf8dbbc4b19b97bdddfde3088fd12a2fdf428fbe
lindenbergannie <lindenbergannie@gmail.com>
2021-11-16 19:16:50 +0300

    done project

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,29 @@
-export const Details = ({ match }) => {
+import axios from 'axios';
+import { IoArrowBack } from 'react-icons/io5';
+import { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { searchByCountry } from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
+
+export const Details = () => {
+  const { name } = useParams();
+  const { push, goBack } = useHistory();
+  const [ country, setCountry ] = useState(null);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => {
+      setCountry(data[0]);
+    })
+  }, [name])
+
   return (
     <div>
-      Details {match.params.name}
+      <Button onClick={goBack}>
+        <IoArrowBack />
+        Back
+      </Button>
+      {country && <Info push={push} {...country} />}
     </div>
   )
 }
